7382dafef96aabf275e2e4d23de7091d7d905f3c
weststarsoft <west@starsoft.games>
2026-04-16 18:28:35 -0300

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -118,4 +118,4 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=300");
   res.status(200).json({ matches });
-}
+} 
